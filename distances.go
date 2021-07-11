9b5a75b9caabf08de8da57c74a4f7f55cc7f6b0e
Sayang Kamil5b <79760043+kamil5b@users.noreply.github.com>
2021-07-12 04:15:56 +0700

    adding cosine and jaccard distance

--- distances.go
+++ distances.go
@@ -1,8 +1,11 @@
 package knnGo
 
-import "math"
+import (
+	"errors"
+	"math"
+)
 
-func MinskowskiDistance(dataset []KNNData, inputAttributes []float64, p int) []KNNData {
+func MinkowskiDistance(dataset []KNNData, inputAttributes []float64, p int) ([]KNNData, error) {
 
 	arr := dataset
 
@@ -10,7 +13,10 @@ func MinskowskiDistance(dataset []KNNData, inputAttributes []float64, p int) []K
 
 	for i := 0; i < len(arr); i++ {
 		if len(arr[i].Attributes) != len(inputAttributes) {
-			return nil
+			return nil, errors.New("input attribute length not is matching with the data attribute length")
+		}
+		if p == 0 {
+			return nil, errors.New("order cannot be 0")
 		}
 		sumAttribute := float64(0)
 		for j := 0; j < len(arr[i].Attributes); j++ {
@@ -25,26 +31,26 @@ func MinskowskiDistance(dataset []KNNData, inputAttributes []float64, p int) []K
 		Distance := math.Pow(sumAttribute, 1/pfloat)
 		arr[i].Distance = Distance
 	}
-	return arr
+	return arr, nil
 }
 
-func EuclideanDistance(dataset []KNNData, inputAttributes []float64) []KNNData {
+func EuclideanDistance(dataset []KNNData, inputAttributes []float64) ([]KNNData, error) {
 
-	return MinskowskiDistance(dataset, inputAttributes, 2)
+	return MinkowskiDistance(dataset, inputAttributes, 2)
 }
 
-func ManhattanDistance(dataset []KNNData, inputAttributes []float64) []KNNData {
+func ManhattanDistance(dataset []KNNData, inputAttributes []float64) ([]KNNData, error) {
 
-	return MinskowskiDistance(dataset, inputAttributes, 1)
+	return MinkowskiDistance(dataset, inputAttributes, 1)
 }
 
-func ChebyshevDistance(dataset []KNNData, inputAttributes []float64) []KNNData {
+func ChebyshevDistance(dataset []KNNData, inputAttributes []float64) ([]KNNData, error) {
 
 	arr := dataset
 
 	for i := 0; i < len(arr); i++ {
 		if len(arr[i].Attributes) != len(inputAttributes) {
-			return nil
+			return nil, errors.New("input attribute length not is matching with the data attribute length")
 		}
 
 		maxMargin := float64(0)
@@ -59,9 +65,70 @@ func ChebyshevDistance(dataset []KNNData, inputAttributes []float64) []KNNData {
 		}
 		arr[i].Distance = maxMargin
 	}
-	return arr
+	return arr, nil
 }
 
-func SupremumDistance(dataset []KNNData, inputAttributes []float64) []KNNData {
+func SupremumDistance(dataset []KNNData, inputAttributes []float64) ([]KNNData, error) {
 	return ChebyshevDistance(dataset, inputAttributes)
+}
+
+func cosineSimilarity(dataAttributes []float64, inputAttributes []float64) float64 {
+	arr := []float64{0, 0, 0}
+	for j := 0; j < len(dataAttributes); j++ {
+		subarr := []float64{
+			dataAttributes[j] * inputAttributes[j],
+			dataAttributes[j] * dataAttributes[j],
+			inputAttributes[j] * inputAttributes[j],
+		}
+		for i := 0; i < len(arr); i++ {
+			arr[i] += subarr[i]
+		}
+	}
+	dataPM := math.Pow(arr[1], 0.5)
+	inputPM := math.Pow(arr[2], 0.5)
+	return (arr[0] / (dataPM * inputPM))
+}
+
+func CosineDistance(dataset []KNNData, inputAttributes []float64) ([]KNNData, error) {
+
+	arr := dataset
+
+	for i := 0; i < len(arr); i++ {
+		if len(arr[i].Attributes) != len(inputAttributes) {
+			return nil, errors.New("input attribute length not is matching with the data attribute length")
+		}
+		cosine := cosineSimilarity(arr[i].Attributes, inputAttributes)
+		arr[i].Distance = 1 - cosine
+	}
+
+	return arr, nil
+}
+
+func jaccardCoefficient(dataAttributes []float64, inputAttributes []float64) float64 {
+	sumMin := float64(0)
+	sumMax := float64(0)
+	for i := 0; i < len(dataAttributes); i++ {
+		if dataAttributes[i] >= inputAttributes[i] {
+			sumMin += inputAttributes[i]
+			sumMax += dataAttributes[i]
+		} else {
+			sumMax += inputAttributes[i]
+			sumMin += dataAttributes[i]
+		}
+	}
+	return sumMin / sumMax
+}
+
+func JaccardDistance(dataset []KNNData, inputAttributes []float64) ([]KNNData, error) {
+
+	arr := dataset
+	//1 - (SUM(MIN) / SUM(MAX))
+	for i := 0; i < len(arr); i++ {
+		if len(arr[i].Attributes) != len(inputAttributes) {
+			return nil, errors.New("input attribute length not is matching with the data attribute length")
+		}
+		jaccardCoef := jaccardCoefficient(arr[i].Attributes, inputAttributes)
+		arr[i].Distance = 1 - jaccardCoef
+	}
+	return arr, nil
 }
--- knnGo.go
+++ knnGo.go
@@ -77,21 +77,28 @@ func MinMaxNormalize(arr []KNNData) {
 func KNNClassification(k int, dataset []KNNData, inputAttributes []float64, distance string, p int) (KNNData, error) {
 
 	var d KNNData
-
+	var err error = nil
 	classes := make([]string, 0)
 	arr := dataset
 	if strings.ToLower(distance) == "euclidean" {
-		arr = EuclideanDistance(dataset, inputAttributes)
+		arr, err = EuclideanDistance(dataset, inputAttributes)
 	} else if strings.ToLower(distance) == "manhattan" {
-		arr = ManhattanDistance(dataset, inputAttributes)
+		arr, err = ManhattanDistance(dataset, inputAttributes)
 	} else if strings.ToLower(distance) == "supremum" {
-		arr = SupremumDistance(dataset, inputAttributes)
+		arr, err = SupremumDistance(dataset, inputAttributes)
 	} else if strings.ToLower(distance) == "chebyshev" {
-		arr = ChebyshevDistance(dataset, inputAttributes)
-	} else if strings.ToLower(distance) == "minskowski" {
-		arr = MinskowskiDistance(dataset, inputAttributes, p)
+		arr, err = ChebyshevDistance(dataset, inputAttributes)
+	} else if strings.ToLower(distance) == "cosine" {
+		arr, err = CosineDistance(dataset, inputAttributes)
+	} else if strings.ToLower(distance) == "jaccard" {
+		arr, err = JaccardDistance(dataset, inputAttributes)
+	} else if strings.ToLower(distance) == "minkowski" {
+		arr, err = MinkowskiDistance(dataset, inputAttributes, p)
 	} else {
-		return KNNData{}, errors.New("invalid distance types or haven't implemented yet")
+		return KNNData{}, errors.New("invalid distance metrics or knnGo haven't implemented yet")
+	}
+	if err != nil {
+		return KNNData{}, err
 	}
 	arr = KNNDataSort(arr, true)
 	arr = arr[:k]
